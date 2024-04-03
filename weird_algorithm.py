37478e2b00752ddd11b455eb0a6334a376426637
EricWithC04 <negativegame117@gmail.com>
2024-04-03 10:18:59 -0300

    comments

--- weird_algorithm.py
+++ weird_algorithm.py
@@ -4,18 +4,19 @@ def weird_algorithm(num):
 
     list_complete = []
 
+    # Bucle que se repetirá de forma indefinida y que se detendra al retornar el resultado final
     while True:
 
         list_complete.append(num)
         
+        # Evaluamos si el numero es par, si lo es lo dividimos a la mitad, caso contrario se multiplica por 3 y se le suma 1
         if num % 2 == 0:
             num = num // 2
         else:
             num = (num * 3) + 1
 
+        # Cuando el ultimo valor de la lista sea 1, retornamos la lista y se termina el bucle.
         if list_complete[-1] == 1:
             return list_complete
 
-numero = 6
-print(weird_algorithm(numero))
-assert weird_algorithm(numero) == [6, 3, 10, 5, 16, 8, 4, 2, 1], "Error, el resultado es distinto a lo esperado"
+assert weird_algorithm(3) == [3, 10, 5, 16, 8, 4, 2, 1], "Error en el caso de prueba"
